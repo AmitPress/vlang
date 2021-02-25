e4f206eda23b9e9167e216ed7549167900dc64d3
AmitPress <amitkrsurjo@gmail.com>
2021-02-26 00:22:19 +0600

    linked-list

---
+++ implementation/singlylinkedlist_one.v
@@ -0,0 +1,72 @@
+// Nod is a compound data type for building SLL
+struct Node {
+mut:
+	id int
+	data int
+	next &Node = 0 // in v you can't directly change the memory
+}
+
+struct Sll {
+mut:
+	init int = 1
+	ids []int
+	size int
+	head &Node = 0
+}
+
+fn (s Sll)showinit(){
+	println(s.init)
+}
+
+fn (mut s Sll) push_back(dt int) {
+	// make a node by the given data
+	dt_node := &Node{
+		id: s.init
+		data : dt
+	}
+	if isnil(s.head) {
+		s.head = dt_node
+	}else{
+		mut ptr := s.head
+		for !isnil(ptr.next) {
+			ptr = ptr.next
+		}
+		ptr.next = dt_node
+	}
+	s.ids << s.init
+	s.init++
+}
+/*
+Node{
+	id: 1
+	data: 30
+	next: &<circular>
+}
+Node{
+    id: 2
+    data: 46
+    next: &<circular>
+}
+Node{
+    id: 3
+    data: 78
+    next: &nil
+}
+*/
+fn (mut s Sll) showall(){
+	mut ptr := s.head
+	for !isnil(ptr) {
+		println(*ptr)
+		ptr = ptr.next
+	}
+}
+
+fn main(){
+	mut l := Sll{}
+	l.push_back(30)
+	l.push_back(46)
+	l.push_back(78)
+	l.showall()
+	l.showinit() // 4
+	println(l.ids) // [1, 2, 3]
+}
